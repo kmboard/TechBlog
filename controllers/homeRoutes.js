ef0e64a72081096663421ce2251e192a61e16f80
Kija <kmboard1@yahoo.com>
2022-05-31 16:50:33 -0400

    trying to fix an error

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -18,7 +18,7 @@ router.get('/', async (req, res) => {
     const blogs = blogData.map((blog) => blog.get({ plain: true }));
 
     // Pass serialized data and session flag into template
-    res.render('blogpage', { 
+    res.render('./login', { 
       blogs, 
       logged_in: req.session.logged_in 
     });
