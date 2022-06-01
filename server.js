df3564b12a64962f279ea548564d2d42ae38b6f7
Kija <kmboard1@yahoo.com>
2022-06-01 18:26:19 -0400

    trying to get routes to work

--- server.js
+++ server.js
@@ -26,13 +26,15 @@ const sess = {
 
 app.use(session(sess));
 
-// Inform Express.js on which template engine to use
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 app.use(routes);
 
